ba0b7455c213a89b3e2a10675b4c619447d0e042
Pawanvaree <156408317+Pawanvaree@users.noreply.github.com>
2026-03-16 15:14:49 +0700

    Add Jest and Supertest for testing; update package.json scripts

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+const http = require("http");
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World from DevOps Lab for EGCO344!\n');
-});
+const requestListener = (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World from DevOps Lab!\n");
+};
 
-server.listen(port, () => {
+const app = http.createServer(requestListener);
+
+// We only listen if this file is run directly, not when imported by tests
+if (require.main === module) {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
     console.log(`Server running at port ${port}`);
-});
+  });
+}
